9cfccd9526e65b2cb81a0b9c81ac12d5e818f157
Will Suto <will.suto@gmail.com>
2024-04-02 18:15:15 -0700

    home infowindow message box

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   AdvancedMarker,
   InfoWindow,
@@ -6,11 +6,21 @@ import {
 } from '@vis.gl/react-google-maps';
 import { useSelector, useDispatch } from 'react-redux';
 
-
 const Home = () => {
   const [infoWindowOpen, setInfoWindowOpen] = useState(false);
   const [markerRef, marker] = useAdvancedMarkerRef();
-  const user = useSelector(state => state.user)
+  const user = useSelector(state => state.user);
+  const [message, setMessage] = useState('');
+
+  const handleMessageChange = (e) => {
+    e.preventDefault();
+    setMessage(e.target.value); // Update message state with the new value from the event
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(message);
+  }
 
   if (user.homeLocationSet) {
     return (
@@ -18,20 +28,41 @@ const Home = () => {
         <AdvancedMarker
           ref={markerRef}
           onClick={() => setInfoWindowOpen(true)}
-          position = {{lat: Number(user.lat), lng: Number(user.lng)}}
+          position={{ lat: Number(user.lat), lng: Number(user.lng) }}
           title={'Home'}
         />
         {infoWindowOpen && (
           <InfoWindow
             anchor={marker}
-            maxWidth={200}
-            onCloseClick={() => setInfoWindowOpen(false)}>
-            This is your home location.
+            maxWidth={'auto'}
+            onCloseClick={() => setInfoWindowOpen(false)}
+          >
+          <form onSubmit={handleSubmit}>
+            <label>Message:
+              <input type="text" value={message} onChange={handleMessageChange}/>
+            </label>
+          </form>
           </InfoWindow>
         )}
-      </>   
+      </>
     )
   }
 };
 
 export default Home;
+
+
+{/* <div>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="message">Message:</label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    value={message}
+                    onChange={handleMessageChange}
+                  ></textarea>
+                </div>
+                <button type="submit">Submit</button>
+              </form>
+            </div> */}
